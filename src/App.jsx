8a3601e8c65b3d4a03ac8b13e824d19dd2b8c844
Matthew Lowe <matthewjlowe11@gmail.com>
2024-10-16 19:30:09 -0400

    Create components folder, create Navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Navigation from './components/Navigation'
 
 function App() {
 
   return (
     <>
-      Let's Setup some servers! Oh YEAH!
+      <Navigation />
     </>
   )
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>Navigation will go here</div>
+  )
+}
+
+export default Navigation
